ff35716153704696c9939c112680bb72f2553c95
Alejandro García <agscm2@gmail.com>
2025-07-17 14:43:26 +0200

    Upload changes

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { signup, login, getTokens } = require("../controllers/authController");
+const { signup, login, getTokens, verifyToken } = require("../controllers/authController");
 const { verifyValidToken } = require("../core/middlewares/auth");
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get('/refresh_token', verifyValidToken, getTokens)
+router.get('/verify_token', verifyValidToken, verifyToken)
 
 // Exports the router
 module.exports = router;
